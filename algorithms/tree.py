e41833fc0f485d5896c046e7530c0dacc331060d
vinfinit <artemenko.vladimi@gmail.com>
2019-01-14 22:07:02 +0300

    Create a tree interface

---
+++ algorithms/tree.py
@@ -0,0 +1,9 @@
+class DecisionTreeRegressor():
+    def __init__(self):
+        print('__init__')
+
+    def fit(self, X, y):
+        print('fit')
+
+    def predict(self, X):
+        print('predict')
