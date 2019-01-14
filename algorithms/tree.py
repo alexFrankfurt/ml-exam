f1c0629f0c698a815445fbe4ab1200600e9b31e2
vinfinit <artemenko.vladimi@gmail.com>
2019-01-14 22:42:15 +0300

    Implementing tree

--- algorithms/tree.py
+++ algorithms/tree.py
@@ -1,8 +1,13 @@
-class DecisionTreeRegressor():
+import numpy as np
+
+class DecisionTreeClassifier():
     def __init__(self):
+        self.classes_ = []
         print('__init__')
 
     def fit(self, X, y):
+        self.classes_ = np.unique(y)
+        
         print('fit')
 
     def predict(self, X):
