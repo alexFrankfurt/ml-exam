342ee7ff8ada0a2f70bc702991d749ddbebe1f7b
vinfinit <artemenko.vladimi@gmail.com>
2019-01-15 00:29:20 +0300

    tree tmp

--- algorithms/tree.py
+++ algorithms/tree.py
@@ -1,5 +1,10 @@
 import numpy as np
 
+class Data():
+    def __init__(self, X, y):
+        self.X = np.array(X)
+        self.y = np.array(y)
+
 class DecisionTreeClassifier():
     def __init__(self):
         self.classes_ = []
@@ -10,21 +15,11 @@ class DecisionTreeClassifier():
     def fit(self, X = [[]], y = []):
         self.classes_ = np.unique(y)
 
-        init_entropy = self.entropy(y)
-        inf_gain_list = []
-        x_split_func_list = []
-        for i in range(0,100):
-            x_split_func = selt._split(X, y)
-            x_split_func_list.push(x_split_func)
-            (y1, y2, _) = x_split_func()
-            entropy_left = self.entropy(y1)
-            entropy_right = self.entropy(y2)
-            inf_gain = init_entropy - (entropy_left + entropy_right)
-            inf_gain_list.push(inf_gain)
+        # data_left = Data(X, y)
+        # (data_left, data_right) = self.createNode(data_left['X'], data_left['y'])
+        # if data_left.shape[0] > 3:
+        #     self.createNode(X, y)
 
-        best_split_arg = np.argmax(inf_gain_list)
-        best_x_split = x_split_func_list[best_split_arg]
-        (_, _, node) = best_x_split()
 
         print('fit')
 
@@ -51,6 +46,25 @@ class DecisionTreeClassifier():
             p_total += p_yes
 
         return p_total
+
+    def createNode(self, X, y):
+        init_entropy = self.entropy(y)
+        inf_gain_list = []
+        x_split_func_list = []
+        for i in range(0,100):
+            x_split_func = selt._split(X, y)
+            x_split_func_list.push(x_split_func)
+            (y1, y2, _) = x_split_func()
+            entropy_left = self.entropy(y1)
+            entropy_right = self.entropy(y2)
+            inf_gain = init_entropy - (entropy_left + entropy_right)
+            inf_gain_list.push(inf_gain)
+
+        best_split_arg = np.argmax(inf_gain_list)
+        best_x_split = x_split_func_list[best_split_arg]
+        (_, _, node) = best_x_split()
+
+        return ()
 
     # function([[x1,x2,x3,y],[x1,x2,x3,y]]) => lambda
     def _split(self, X, y):
