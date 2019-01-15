910ed4eb332be9143ab132ca11705d1ba9fac301
alexFrankfurt <alex_frankfurt@mail.ru>
2019-01-15 03:25:35 +0300

    create random forest

--- algorithms/forest.py
+++ algorithms/forest.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from tree import DecisionTreeClassifier
+from random import sample
 
 class RandomForestRegressor():
     # independent variables of training set
@@ -16,7 +17,22 @@ class RandomForestRegressor():
 
      def __init__(self, x, y, n_trees, n_features, sample_sz, depth=10, min_leaf=2):
          tree_list = []
-         for i in range(n_trees):
+         for i in range(0, n_trees):
+            random_features = []
+            random_rows     = []
+            random_y        = []
+
+            rand_feature_idxs = sample(range(0, len(x[0])), n_features)
+            for features in x:
+                random_features.append([features[i] for i in rand_feature_idxs])
+
+            rand_row_idxs = sample(range(0, len(x)), sample_sz)
+            for idx, features in enumerate(x):
+                if idx in rand_row_idxs:
+                    random_rows.append(features)
+                    random_y.append(y[idx])
+
+
             tree = DecisionTreeClassifier(depth, min_leaf)
-            tree.fit(x, y)
+            tree.fit(random_rows, random_y)
             tree_list.append(tree)
