7813fc77db4f07141d48c569a38ab887824fb978
vinfinit <artemenko.vladimi@gmail.com>
2019-01-15 10:02:02 +0300

    Add accuracy

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ iris = load_iris()
 data1 = pd.DataFrame(data= np.c_[iris['data'], iris['target']],
                      columns= iris['feature_names'] + ['target'])
 
-x = data1.sample(20).values
+x = data1.sample(50).values
 min_max_scaler = preprocessing.MinMaxScaler()
 x_scaled = min_max_scaler.fit_transform(x)
 df = pd.DataFrame(x_scaled)
@@ -27,5 +27,10 @@ tree_classifier = tree.DecisionTreeClassifier()
 tree_model = tree_classifier.fit(x_iris, y_iris)
 tree_classifier.render()
 
-res = tree_model.predict(x_iris)
-print('res:', res)
+y_ = tree_model.predict(x_iris)
+print('y_:', y_)
+
+res = y_iris == y_
+# print('res:', res)
+acc = np.sum(res) / len(y_)
+print('accuracy:', acc)
